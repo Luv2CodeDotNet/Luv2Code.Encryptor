c61789808c0bf332aeb80d483e559c36f63c26aa
Ruslan Boltersdorf <r.b.vs@outlook.de>
2021-08-20 00:57:12 +0200

    extensions added

---
+++ Luv2Code.Encryptor/Services/EnigmaExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Luv2Code.Encryptor.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EnigmaExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Encrypt(this string value, string key)
+        {
+            return Enigma.Encrypt( key, value);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cipherText"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Decrypt(this string cipherText, string key)
+        {
+            return Enigma.Decrypt( key, cipherText);
+        }
+    }
+}
